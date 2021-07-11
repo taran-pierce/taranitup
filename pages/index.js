7b6dac97e6c5ba6550f2b4c3330c60dadbb0a969
Taran Pierce <taran.tpdesign@gmail.com>
2021-07-11 07:40:08 -0600

    feature/basic-page-structure
    minor text update

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,10 @@ export default function Index() {
           content={`Just a place for me to do some messing around with stuff. This site is hooked up to various technologies and I will explain how each of them are hooked up.`}
         />
         <Text
-          content={`Right now this site is using Static Site Generation from Next.js and being hosted through AWS using CodeBuild and Route 53.`}
+          content={`Right now this site is very simple but is using nodejs running Nextjs to leverage their Static Site Generation. I'm using services from AWS like CodeBuild to grab the repo and build the application. Moving the artifact it creates to an S3 bucket where I have the DNS managed from Route 53.`}
+        />
+        <Text
+          content={`I will be hooking up more things as I get time!`}
         />
      </Layout>
     </>
