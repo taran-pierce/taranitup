57dfb8ce28327ad12693e8c7cb6f22e8a29e8278
Taran Pierce <taran.tpdesign@gmail.com>
2024-03-04 18:01:03 -0700

    feature/adjust-build
    adjust build config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
-module.exports = {
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/_next/:js*',
+//         locale: false,
+//         headers: [
+//           {
+//             key: 'Cache-Control',
+//             value: 'public, max-age=9999999999, must-revalidate',
+//           }
+//         ],
+//       },
+//       {
+//         source: '/:all*(svg|jpg|png)',
+//         locale: false,
+//         headers: [
+//           {
+//             key: 'Cache-Control',
+//             value: 'public, max-age=9999999999, must-revalidate',
+//           }
+//         ],
+//       },
+//     ];
+//   }
+// }
+
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
   async headers() {
     return [
       {
@@ -22,5 +53,8 @@ module.exports = {
         ],
       },
     ];
-  }
+  },
+  distDir: 'out/',
 }
+ 
+module.exports = nextConfig;
